6c9baaf050d57e4100db4aa45375d7b33472dd45
JOSEPH YARIJARLA <josephyarijarla@gmail.com>
2023-11-19 19:56:41 -0600

    AddTwoNumbers from problemlist

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *beginValue = nullptr;
+        bool carry = false;
+        while((!(l1 == nullptr && l2 == nullptr)) || carry){
+            ListNode *nextNode = new ListNode();
+
+            //Summation
+            int sum = (l1 == nullptr ? 0 : l1->val) +
+                (l2 == nullptr ? 0 : l2->val) + 
+                (carry ? 1 : 0);
+            nextNode->val = sum%10;
+            carry = sum > 9;
+
+            //Add to list
+            if(beginValue == nullptr)
+                beginValue = nextNode;
+            else{
+                ListNode *traverseNode = beginValue;
+                while(traverseNode->next != nullptr)
+                    traverseNode = traverseNode->next;
+                traverseNode->next = nextNode;
+            }
+            if(l1 != nullptr)
+                l1 = l1->next;
+            if(l2 != nullptr)
+                l2 = l2->next;
+
+        } 
+        return beginValue;    
+    }
+};
